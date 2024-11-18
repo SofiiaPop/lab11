6b3fa3285b82070ac0d3b5138906d1f23dbcee6f
Sofiia Popeniuk <146963804+SofiiaPop@users.noreply.github.com>
2024-11-18 22:26:24 +0200

    Delete lab111 directory

--- lab111/src/main/java/ua/edu/ucu/apps/Task2/MailCode/MailCode.java
+++
@@ -1,7 +0,0 @@
-package ua.edu.ucu.apps.Task2.MailCode;
-
-import ua.edu.ucu.apps.Task2.Client;
-
-public interface MailCode {
-    public String generate(Client client);
-}
--- lab111/src/main/java/ua/edu/ucu/apps/Task3/MyImage.java
+++
@@ -1,5 +0,0 @@
-package ua.edu.ucu.apps.Task3;
-
-public interface MyImage {
-    void display();
-}
