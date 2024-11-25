accdbfbbaad85c3818379a2f62818d550c281718
Sofiia Popeniuk <popeniuk.pn@ucu.edu.ua>
2024-11-25 09:52:18 +0200

    correct

--- src/main/java/ua/edu/ucu/apps/Task3/Main.java
+++ src/main/java/ua/edu/ucu/apps/Task3/Main.java
@@ -2,7 +2,7 @@ package ua.edu.ucu.apps.Task3;
 
 public class Main {
     public static void main(String[] args) {
-        MyImage img = new ProxyImage("sofiiapopeniuk/Downloads/photo_2024-11-11 9.50.47 AM.jpeg");
+        MyImage img = new ProxyImage("/Users/sofiiapopeniuk/Downloads/photo_2024-11-11 9.50.47 AM.jpeg");
         System.out.println("Image created but not loaded yet.");
         img.display();
     }
